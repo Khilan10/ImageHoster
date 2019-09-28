9620da8c14a63762868fefe89083c75c687664e1
Khilan10 <mahajankhilan1@gmail.com>
2019-09-29 05:00:51 +0530

    fixing issue:fixed issue that only the person who post the image can edit it by branch1_fixing_issues

--- src/main/java/ImageHoster/controller/ImageController.java
+++ src/main/java/ImageHoster/controller/ImageController.java
@@ -92,13 +92,31 @@ public class ImageController {
     //The method first needs to convert the list of all the tags to a string containing all the tags separated by a comma and then add this string in a Model type object
     //This string is then displayed by 'edit.html' file as previous tags of an image
     @RequestMapping(value = "/editImage")
-    public String editImage(@RequestParam("imageId") Integer imageId, Model model) {
+    public String editImage(@RequestParam("imageId") Integer imageId, Model model,HttpSession session) {
         Image image = imageService.getImage(imageId);
 
+        //adding the error message to show to user if he/she tries to edit other user image
+        String error = "Only the owner of the image can edit the image";
         String tags = convertTagsToString(image.getTags());
+
+        //this will be used to get user details of the user who has logged in
+        User loggedUser = (User) session.getAttribute("loggeduser");
+        //this will be used to get the information of the image user
+        User imageUser = image.getUser();
         model.addAttribute("image", image);
-        model.addAttribute("tags", tags);
-        return "images/edit";
+
+        //condition added such that if the loogged in user and the user who uploaded the image are same than
+        //the image will get edited otherwise message will be shown to user
+        if (imageUser.getId() == loggedUser.getId()) {
+            model.addAttribute("tags", tags);
+            return "images/edit";
+        } else {
+            model.addAttribute("tags",image.getTags());
+            model.addAttribute("editError",error);
+            return "images/image";
+        }
+
+
     }
 
     //This controller method is called when the request pattern is of type 'images/edit' and also the incoming request is of PUT type
